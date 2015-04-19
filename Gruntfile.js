961cafb8e79464e7c88a9b431b4821466b5448f5
Charles King <chip@cwking.com>
2015-04-19 12:21:20 -0500

    Adds grunt serve task

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,17 +22,57 @@ module.exports = function (grunt) {
             }
         },
 
+        connect: {
+            server: {
+                options: {
+                    base: config.output.dir,
+                    protocol: config.dev.server.protocol,
+                    hostname: config.dev.server.hostname,
+                    port: config.dev.server.port,
+                    open: true,
+                    livereload: config.dev.server.livereload
+                }
+            }
+        },
+
         copy: {
             index: {
                 files: [{
-                    cwd: config.input.dir,
                     src: config.input.html.internal,
                     dest: config.output.dir,
-                    expand: true
+                    expand: true,
+                    flatten: true
                 }]
+            }
+        },
+
+        watch: {
+
+            dist: {
+                files: config.output.dir + '/*.*',
+                tasks: [],
+                options: {
+                    livereload: config.dev.server.livereload
+                }
+            },
+
+            copyIndex: {
+                files: config.input.html.internal,
+                tasks: 'copy:index',
+            },
+
+            concatInternal: {
+                files: config.input.js.internal,
+                tasks: 'concat:internal'
+            },
+
+            concatExternal: {
+                files: config.input.js.external,
+                tasks: 'concat:external'
             }
         }
     });
 
     grunt.registerTask('compile', ['clean', 'copy', 'concat']);
+    grunt.registerTask('serve', ['compile', 'connect', 'watch']);
 };
--- project.conf.js
+++ project.conf.js
@@ -1,7 +1,7 @@
 var dirs = {
-    output: 'dist',
-    source: 'src',
-    npm: 'node_modules'
+    output: 'dist/',
+    source: 'src/',
+    npm: 'node_modules/'
 };
 
 module.exports = {
@@ -9,25 +9,36 @@ module.exports = {
     output: {
         dir: dirs.output,
         js: {
-            internal: dirs.output + '/internal.js',
-            external: dirs.output + '/external.js'
+            internal: dirs.output + 'internal.js',
+            external: dirs.output + 'external.js'
         }
     },
 
     input: {
         dir: dirs.source,
         html: {
-            internal: ['index.html']
+            internal: [
+                dirs.source + 'index.html'
+            ]
         },
         js: {
             internal: [
-                dirs.source + '/**/*.module.js',
-                dirs.source + '/**/*.js',
+                dirs.source + '**/*.module.js',
+                dirs.source + '**/*.js',
                 '!**/*.spec.js'
             ],
             external: [
-                dirs.npm + '/angular/angular.js'
+                dirs.npm + 'angular/angular.js'
             ]
+        }
+    },
+
+    dev: {
+        server: {
+            protocol: 'http',
+            hostname: 'localhost',
+            port: 8000,
+            livereload: true
         }
     }
 };
