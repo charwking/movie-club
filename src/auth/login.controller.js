245390c35d042a501b421871e78e49fa896729c3
Charles King <chip@cwking.com>
2015-06-13 13:21:23 -0400

    Add login spinner and error message

--- src/auth/login.controller.js
+++ src/auth/login.controller.js
@@ -8,15 +8,25 @@
     function LoginController($state, authApi) {
         var vm = this;
         vm.login = login;
+        vm.isLoading = false;
+        vm.loginFailed = false;
 
         function login() {
             if (!vm.loginForm.$valid) {
                 return;
             }
 
+            vm.isLoading = true;
+            vm.loginFailed = false;
+
             authApi.login(vm.email, vm.password)
                 .then(function () {
                     $state.go('dashboard');
+                })
+                .catch(function () {
+                    vm.password = "";
+                    vm.isLoading = false;
+                    vm.loginFailed = true;
                 });
         }
     }
