f7e262a12a05b6abfc9c27c0c989e70108f12b73
charwking <chip.king@gmail.com>
2015-07-02 17:01:46 -0500

    Dynamically choose firebase instance to use

--- src/firebase/firebaseRef.service.js
+++ src/firebase/firebaseRef.service.js
@@ -5,8 +5,16 @@
         .module('movieClub')
         .factory('firebaseRef', firebaseRef);
 
-    function firebaseRef() {
-        return new Firebase('https://glowing-inferno-1828.firebaseio.com/');
+    function firebaseRef(environmentService) {
+
+        var env = environmentService.get();
+
+        var firebaseUrl =
+            (env === environmentService.LOCAL || env === environmentService.DEV) ?
+            'https://glowing-inferno-1828.firebaseio.com/' :
+            'https://movie-club.firebaseio.com/';
+
+        return new Firebase(firebaseUrl);
     }
 
 }(window.angular, window.Firebase));
