279837ba70e8890ed6d83a6bc43eb71ee8c81688
charwking <chip.king@gmail.com>
2015-07-02 20:40:10 -0500

    Dont set times on dirs

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ export TRAVIS_BRANCH="master"
 export TRAVIS_PULL_REQUEST="false"
 
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    rsync -arz --delete --stats --rsh="sshpass -p $DEPLOY_PASSWORD ssh -l $DEPLOY_USER" dist/ cwking.com:$DEPLOY_PATH;
+    rsync -arzO --delete --stats --rsh="sshpass -p $DEPLOY_PASSWORD ssh -l $DEPLOY_USER" dist/ cwking.com:$DEPLOY_PATH;
 fi
