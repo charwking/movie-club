d0c8ee0fad9475668fc42b60345af8ef07cb8815
charwking <chip.king@gmail.com>
2015-07-02 20:44:20 -0500

    Remove vars

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ev
 
-export TRAVIS_BRANCH="master"
-export TRAVIS_PULL_REQUEST="false"
-
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     rsync -arzO --delete --stats --rsh="sshpass -p $DEPLOY_PASSWORD ssh -l $DEPLOY_USER" dist/ cwking.com:$DEPLOY_PATH;
 fi
