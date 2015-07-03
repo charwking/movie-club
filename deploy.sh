bfde915b4e9f14c0d262f0e38f26bee90b2454d6
charwking <chip.king@gmail.com>
2015-07-02 20:09:08 -0500

    Add spaces

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -ev
 export TRAVIS_BRANCH="master"
 export TRAVIS_PULL_REQUEST="false"
 
-if [["$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false"]]; then
+if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     sudo apt-get -y -qq install sshpass;
     rsync -arz --delete --stats --rsh="sshpass -p $DEPLOY_PASSWORD ssh -l $DEPLOY_USER" dist/ cwking.com:$DEPLOY_PATH;
 fi
