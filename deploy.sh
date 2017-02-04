d3c3b3a135849ce240e9c77a010e2d15f0cf4d84
Charles William King <charwking@users.noreply.github.com>
2017-02-03 20:05:57 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 set -ev
 
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    sshpass -p $DEPLOY_PASSWORD rsync -arz0 --delete --stats dist/ $DEPLOY_USER@cwking.com:$DEPLOY_PATH;
+    rsync -arzO --delete --stats --rsh="sshpass -p $DEPLOY_PASSWORD ssh -l $DEPLOY_USER" dist/ cwking.com:$DEPLOY_PATH;
 fi
