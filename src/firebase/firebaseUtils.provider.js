fe17e8a4b528efa8d2dc9d29aa0edfa2f895da2b
charwking <chip.king@gmail.com>
2017-04-16 16:32:09 -0500

    Add getArray and getObject to firebaseUtils

--- src/firebase/firebaseUtils.provider.js
+++ src/firebase/firebaseUtils.provider.js
@@ -32,20 +32,29 @@
         function firebaseUtils($firebaseArray, $firebaseObject, firebaseRefFactory) {
 
             return {
+                getArray: getArray,
+                getObject: getObject,
                 promiseArray: promiseArray,
                 promiseObject: promiseObject
             };
 
-            function promiseArray(path) {
+            function getArray(path) {
                 var ref = firebaseRefFactory.getRef(path);
-                return $firebaseArray(ref).$loaded();
+                return $firebaseArray(ref);
+            }
+
+            function getObject(path) {
+                var ref = firebaseRefFactory.getRef(path);
+                return $firebaseObject(ref);
+            }
+
+            function promiseArray(path) {
+                return getArray(path).$loaded();
             }
 
             function promiseObject(path) {
-                var ref = firebaseRefFactory.getRef(path);
-                return $firebaseObject(ref).$loaded();
+                return getObject(path).$loaded();
             }
         }
     }
-
 }());
--- src/firebase/firebaseUtils.service.spec.js
+++ src/firebase/firebaseUtils.service.spec.js
@@ -27,6 +27,34 @@
             });
         });
 
+        describe('getArray', function () {
+
+            it('gets a firebase array', function () {
+                firebaseRefFactory.getRef.and.returnValue('firebase ref');
+                $firebaseArrayMock.and.returnValue(firebasePromiseMock);
+
+                var result = subject.getArray('firebase path');
+
+                expect(firebaseRefFactory.getRef).toHaveBeenCalledWith('firebase path');
+                expect($firebaseArrayMock).toHaveBeenCalledWith('firebase ref');
+                expect(result).toEqual(firebasePromiseMock);
+            });
+        });
+
+        describe('getObject', function () {
+
+            it('gets a firebase object', function () {
+                firebaseRefFactory.getRef.and.returnValue('firebase ref');
+                $firebaseObjectMock.and.returnValue(firebasePromiseMock);
+
+                var result = subject.getObject('firebase path');
+
+                expect(firebaseRefFactory.getRef).toHaveBeenCalledWith('firebase path');
+                expect($firebaseObjectMock).toHaveBeenCalledWith('firebase ref');
+                expect(result).toEqual(firebasePromiseMock);
+            });
+        });
+
         describe('promiseArray', function () {
 
             it('gets a promise from a $firebaseArray', function () {
