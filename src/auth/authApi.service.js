063ce89885d3c234c66167537d3b7e5b0984d874
charwking <chip.king@gmail.com>
2017-04-16 19:35:11 -0500

    authApi refactoring

--- src/auth/authApi.service.js
+++ src/auth/authApi.service.js
@@ -1,72 +1,40 @@
-(function (angular) {
+(function () {
     'use strict';
 
     angular
         .module('movieClub')
         .factory('authApi', authApi);
 
-    function authApi($firebaseAuthService, $q, firebaseUtils) {
-        var factory = {
-                login: login,
-                logout: logout,
-                register: register,
-                isLoggedIn: isLoggedIn,
-                isAdmin: isAdmin
-            },
-            currentUserId,
-            isAdminFlag,
-            authRef = $firebaseAuthService;
-
-        init();
-        return factory;
-
-        function init() {
-            recordUser(authRef.$getAuth());
-        }
-
-        function recordUser(authData) {
-            if (authData && authData.uid) {
-                currentUserId = authData.uid;
-                return firebaseUtils.promiseObject('adminStore').then(function (adminStore) {
-                    isAdminFlag = adminStore[currentUserId];
-                    return authData;
-                });
-            }
-        }
+    function authApi($firebaseAuthService, firebaseUtils) {
+        return {
+            login: login,
+            logout: logout,
+            register: register
+        };
 
         function login(email, password) {
-            return authRef
-                .$signInWithEmailAndPassword(email, password)
-                .then(recordUser);
+            return $firebaseAuthService
+                .$signInWithEmailAndPassword(email, password);
         }
 
         function logout() {
-            currentUserId = null;
-            isAdminFlag = false;
-            authRef.$signOut();
+            $firebaseAuthService.$signOut();
         }
 
         function register(username, email, password) {
-            return authRef.$createUserWithEmailAndPassword(email, password)
+            return $firebaseAuthService
+                .$createUserWithEmailAndPassword(email, password)
                 .then(_.partial(login, email, password))
                 .then(_.partialRight(addUsername, username));
         }
 
         function addUsername(auth, username) {
-            return firebaseUtils.promiseObject(['users', auth.uid])
+            return firebaseUtils
+                .promiseObject(['users', auth.uid])
                 .then(function (user) {
                     user.username = username;
                     return user.$save();
                 });
         }
-
-        function isLoggedIn() {
-            return !!currentUserId;
-        }
-
-        function isAdmin() {
-            return isAdminFlag;
-        }
     }
-
-}(window.angular));
+}());
