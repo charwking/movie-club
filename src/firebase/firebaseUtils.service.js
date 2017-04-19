28c49b09ecf08845c8ffc88f0d0d92b407a93461
charwking <chip.king@gmail.com>
2017-04-18 20:13:07 -0500

    Convert firebaseUtils provider to factory

---
+++ src/firebase/firebaseUtils.service.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('movieClub')
+        .factory('firebaseUtils', firebaseUtils);
+
+    function firebaseUtils($firebaseArray, $firebaseObject, firebaseRefFactory) {
+
+        return {
+            getArray: getArray,
+            getObject: getObject,
+            promiseArray: promiseArray,
+            promiseObject: promiseObject
+        };
+
+        function getArray(path) {
+            var ref = firebaseRefFactory.getRef(path);
+            return $firebaseArray(ref);
+        }
+
+        function getObject(path) {
+            var ref = firebaseRefFactory.getRef(path);
+            return $firebaseObject(ref);
+        }
+
+        function promiseArray(path) {
+            return getArray(path).$loaded();
+        }
+
+        function promiseObject(path) {
+            return getObject(path).$loaded();
+        }
+    }
+}());
